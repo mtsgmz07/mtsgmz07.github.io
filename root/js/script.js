abf6d05523c52573899d99e71370033bd3277b0c
mtsgmz07 <mgomez@edenschool.fr>
2024-10-04 13:54:19 +0200

    commit

---
+++ root/js/script.js
@@ -0,0 +1,9 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        }
+    });
+});
+
+observer.observe(document.querySelector(".associate-group"))
